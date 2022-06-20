a57b59ad14596dfcc9ea6cc17bc595daa4a3d586
AvresCode <servas230@gmail.com>
2022-06-20 17:11:51 +0200

    Completed all functions

--- index2.js
+++ index2.js
@@ -2,10 +2,43 @@ function calculateBMI(weight, height){
     return weight / (height * height);
      
 }
+
 function calculateBMR(weight, height, ageOfUser, genderOfUser){
     const heighInCm = height * 100;
-    const BMR = genderOfUser === "m" ? (10 * weight) + (6.25 * heighInCm)- (5 * ageOfUser) + 50 : (10 * weight) + (6.25 * heighInCm)- (5 * ageOfUser) -150;
-    return BMR
+   return genderOfUser === "m" ? (10 * weight) + (6.25 * heighInCm)- (5 * ageOfUser) + 50 : (10 * weight) + (6.25 * heighInCm)- (5 * ageOfUser) -150;
+   
+}
+
+function calculateIdealWeight(height){
+    const idealBMI = 22.5;
+return idealWeight = idealBMI * height * height;
+
+
+}
+
+ function calculateDailyCalories(exercise, BMRamount){
+    return exercise === "yes" ? BMRamount * 1.6 : BMRamount * 1.4;
+
+}
+function calculateWeightDifference(weight, personIdealWeight){
+    return weight- personIdealWeight;
+    
+}
+
+function calculateDietWeeks(weightVariance){
+   return Math.abs(weightVariance / 0.5);
+
+}
+
+function calculateDietCalories(weightVariance, dailyCalorie){
+    
+if(weightVariance > 0){
+    return dailyCalorie- 500;
+}else if(weightVariance< 0){
+    return dailyCalorie + 500;
+}
+
+
 }
 
 function bmiCalculator(){
@@ -17,10 +50,13 @@ const dailyExercise = process.argv[5];
 const gender = process.argv[6];
 
 const BMI = calculateBMI(weightInKg, heightInM);
-console.log('BMI in main:', BMI);
-
 const BMR = calculateBMR(weightInKg, heightInM, age, gender);
+const idealWeight = calculateIdealWeight(heightInM);
+const calorieNeededPerDay = calculateDailyCalories(dailyExercise, BMR);
 
+const weightDifference = calculateWeightDifference(weightInKg, idealWeight);
+const weeksToDiet = calculateDietWeeks(weightDifference);
+const calorieWhileDieting = calculateDietCalories(weightDifference, calorieNeededPerDay);
 
  console.log("WEIGHT: ", weightInKg);
  console.log("HEIGHT: ", heightInM);
